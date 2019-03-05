4bc28acb357ef3aaf3bce9bf690a2fdf2aace60f
RyanCarlson060 <rc323rv6>
2019-03-04 20:21:56 -0800

    Add test of dataParser and toString for ElectionResults

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /***
  *
@@ -10,7 +11,12 @@ public class Main {
     public static void main(String[] args) {
         // Test of utils
         String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
-        System.out.println(data);
+       // System.out.println(data);
+
+
+        ArrayList<ElectionResult> electionResults =Utils.parse2016PresidentialResults(data);
+        System.out.println(electionResults.get(0));
+
     }
 
 }
