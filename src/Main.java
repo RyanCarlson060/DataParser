10738a611d61a6c5ead2ee0f54f026ff2095df6f
RyanCarlson060 <rc323rv6>
2019-03-10 14:16:54 -0700

    Add tests for creating a DataManager object in Main

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         // Test of utils
-        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
-       // System.out.println(data);
-
-
-        ArrayList<ElectionResult> electionResults =Utils.parse2016PresidentialResults(data);
-        System.out.println(electionResults.get(0));
-
+        String ElectionData = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        String EducationData = Utils.readFileAsString("data/Education.csv");
+        String EmploymentData = Utils.readFileAsString("data/Unemployment.csv");
+       // System.out.println(ElectionData);
+        //ArrayList<ElectionResult> electionResults = Utils.parse2016PresidentialResults(ElectionData);
+        //System.out.println(electionResults.get(0));
+        DataManager d = Utils.parseFilesIntoDataManager(ElectionData, EducationData, EmploymentData);
+        System.out.println(d.getStates().get(0).getCounties().get(0).getEduc2016().getOnlyHighSchool());
     }
 
 }
