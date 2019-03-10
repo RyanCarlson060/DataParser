2a2b38c05ac3a3da09b597f4489ede3e4c46073c
RyanCarlson060 <rc323rv6>
2019-03-10 10:06:38 -0700

    Add DataManager class with getters and setters

---
+++ src/DataManager.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class DataManager {
+
+    private ArrayList<State> states;
+
+    public DataManager(ArrayList<State> states) {
+        this.states = states;
+    }
+
+    public ArrayList<State> getStates() {
+        return states;
+    }
+
+    public void setStates(ArrayList<State> states) {
+        this.states = states;
+    }
+}
