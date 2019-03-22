a82dbefffa9278871e5f5c2bcc7968763e17550a
rcarlson060 <rcarlson060@student.fuhsd.org>
2019-03-22 10:38:58 -0700

    Added testing in main for parsing

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
        // System.out.println(ElectionData);
         //ArrayList<ElectionResult> electionResults = Utils.parse2016PresidentialResults(ElectionData);
         //System.out.println(electionResults.get(0));
-        DataManager d = Utils.parseFilesIntoDataManager(ElectionData, EducationData, EmploymentData);
+        DataManager d = Utils.parseFilesForCSV(ElectionData, EducationData, EmploymentData);
         System.out.println(d.getStates().get(0).getCounties().get(0).getEduc2016().getOnlyHighSchool());
     }
 
