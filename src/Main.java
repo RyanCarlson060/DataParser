e3d37823eaf270e20167d0486fba5efc0d96b521
RyanCarlson060 <rc323rv6>
2019-03-24 11:14:20 -0700

    Added tests for dataparser in Main

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,13 @@ public class Main {
        // System.out.println(ElectionData);
         //ArrayList<ElectionResult> electionResults = Utils.parse2016PresidentialResults(ElectionData);
         //System.out.println(electionResults.get(0));
-        DataManager d = Utils.parseFilesIntoDataManager(ElectionData, EducationData, EmploymentData);
-        System.out.println(d.getStates().get(0).getCounties().get(0).getEduc2016().getOnlyHighSchool());
+        DataManager d = Utils.parseFilesForCSV(ElectionData, EducationData, EmploymentData);
+        System.out.println(d.getStates().get(0).getCounties().get(0).getUnemploymentRate());
+
+        ArrayList<City> cities = Utils.getNonprofits();
+        System.out.println(cities);
+
+
     }
 
 }
